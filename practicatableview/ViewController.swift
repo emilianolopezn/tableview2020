af88201178c923535f02c561df05c3ab04219e5c
Emiliano Mawe Macbook <emiliano@mawetecnologias.com>
2020-11-03 13:18:51 -0700

    Initial Commit

---
+++ practicatableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practicatableview
+//
+//  Created by Emiliano Lopez on 11/3/20.
+//  Copyright © 2020 Emiliano Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
